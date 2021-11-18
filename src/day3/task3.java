bae82feef1369cbeac0f065c9322e9e4b2bd8838
Sharipov Airat <sharipov.airat@gmail.com>
2021-11-18 13:03:54 +0300

    Rename Project

--- src/day3/task3.java
+++ src/day3/task3.java
@@ -11,18 +11,17 @@ import java.util.Scanner;
 
 public class task3 {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         for (int i = 0; i < 5; i++) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+            }
+            System.out.println(a / b);
 
-
-      double a = sc.nextDouble();
-      double b = sc.nextDouble();
-      if (b==0){
-          System.out.println("Деление на 0");
-      }
-            System.out.println(a/b);
-
-      }
+        }
 
     }
 }
