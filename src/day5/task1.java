c4ab88693daae354eaea2cfd8162b253ed7560c1
Sharipov Airat <sharipov.airat@gmail.com>
2021-11-19 22:40:11 +0300

    Rename Project

--- src/day5/task1.java
+++ src/day5/task1.java
@@ -5,11 +5,47 @@
 
 package day5;
 
-public class task1 {
-        public static class Car{
-                int year;
-                String color;
-                String model;
 
+public class task1 {
+
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setYear(1942);
+        car1.setColor("black");
+        car1.setModel("Ford");
+        System.out.println("Цвет: " + car1.getColor());
+        System.out.println("Год: " + car1.getYear());
+        System.out.println("Модель: " + car1.getModel());
+    }
+
+
+    public static class Car {
+        private int year;
+        private String color;
+        private String model;
+
+        public void setYear(int year) {
+            this.year = year;
         }
+
+        public int getYear() {
+            return year;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public String getModel() {
+            return model;
+        }
+    }
 }
